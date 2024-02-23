88b36b68b9ba88b0bf955bebf6f2f52b9026a64e
vaxtangi123 <argutasjvikiv@gmail.com>
2024-02-23 17:48:46 +0400

    commit

---
+++ day1/homework/lesson1.py
@@ -0,0 +1,52 @@
+name = "vato"
+last_name = "arghutashvili"
+age = 15
+height =1.73
+eyes_color = "aqua"
+print(name,last_name,age,height,eyes_color)
+
+
+
+
+print(5+5)
+print(10+5)
+print(40+20)
+print(52+24)
+print(20+23)
+
+
+
+print(30*2)
+print(20*4)
+print(23*6)
+print(230*43)
+print(323*32)
+
+
+
+print(30//2)
+print(100//5)
+print(35//3)
+print(45//5)
+print(36//3)
+
+
+
+
+print("anaas dance score=96")
+
+
+
+
+
+
+
+
+
+
+
+
+
+("hello world")
+5
+5.2
