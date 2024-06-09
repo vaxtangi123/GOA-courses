f9da6a464e2329dd3a1655ba20f68f90b38298a9
vaxtangi123 <argutasjvikiv@gmail.com>
2024-06-09 22:44:37 +0400

    second commit

---
+++ level01/homework/lesson1.py
@@ -0,0 +1,15 @@
+
+friend_name1 = ("andria")
+friend_name2 = ("giorgi")
+friend_name3 = ("vato")
+friend_name4 = ("nika")
+
+mother_Neme = ("natia")
+father_name = ("vaxushti")
+
+fav_food = ("burgeri, shaurma")
+print(friend_name1, friend_name2, friend_name3, friend_name4,mother_Neme, father_name, fav_food)
+
+
+
+
